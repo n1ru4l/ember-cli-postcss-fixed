316c62ebb455e335120741b44e28016d85728b0b
Laurin Quast <laurinquast@googlemail.com>
2015-10-12 11:22:33 +0200

    *Removed unuused imports *Updated dependencies

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a modification of https://www.npmjs.com/package/ember-cli-postcss
 
-It adds functionality for multiple file processing.
+It adds functionality for multiple file processing and for preprocessing using [node-sass](https://www.npmjs.com/package/node-sass).
 
 Use [postcss](https://github.com/postcss/postcss) to process your `css` with a large selection of JavaScript plug-ins.
 
@@ -83,4 +83,69 @@ If you need to process multiple files, it can be done by [configuring the output
   }
 });
 
+```
+
+### Preprocessing files with Sass
+If you need to preprocess your files with [node-sass](https://www.npmjs.com/package/node-sass) you can enable it in your ember-cli-build.js:
+```javascript
+	var app = new EmberApp({
+        postcssOptions: {
+          plugins: [
+            {
+              module: autoprefixer,
+              options: {
+                browsers: ['last 2 version']
+              }
+            }
+        ],
+        useSass: true
+        }
+      });
+```
+By default this ember-cli-postcss-fixed will process .css files.
+However you can specify the file extension like this:
+```javascript
+	var app = new EmberApp({
+        postcssOptions: {
+          plugins: [
+            {
+              module: autoprefixer,
+              options: {
+                browsers: ['last 2 version']
+              }
+            }
+        ],
+        useSass: true,
+        extension: 'scss'
+        }
+      });
+```
+
+If you want to import files outside your styles folder you can include them like this:
+
+```javascript
+  var app = new EmberApp(defaults, {
+    // Add options here
+    postcssOptions: {
+        useSass: true,
+        extension: 'scss',
+        plugins: [
+            {
+                module: require('autoprefixer'),
+                options: {
+                    browsers: ['last 2 versions']
+                }
+            }
+        ],
+        includePaths: [
+            'bower_components/bootstrap-sass/assets/stylesheets'
+        ]
+    }
+  });
+```
+
+After including you can import the files like this 
+
+```scss
+@import "bootstrap"; //Imports 'bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss'
 ```
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var path = require('path');
 var mergeTrees = require('broccoli-merge-trees');
 var merge = require('merge');
 var fs = require('fs');
-var lost = require('lost');
 
 var mkdirp = require('mkdirp');
 var assign = require("object-assign");
@@ -11,67 +10,88 @@ var CachingWriter = require('broccoli-caching-writer');
 var postcss = require('postcss');
 var CssSyntaxError = require('postcss/lib/css-syntax-error');
 
+//Sass
+var sass = require('node-sass');
+
 //rip of broccoli-postcss
-function PostcssCompiler (inputTrees, inputFile, outputFile, plugins, map) {
-    if ( !(this instanceof PostcssCompiler) ) {
-        return new PostcssCompiler(inputTrees, inputFile, outputFile, plugins, map);
-    }
+function PostcssCompiler(inputTrees, inputFile, outputFile, plugins, map, useSass, includePaths) {
+	if (!(this instanceof PostcssCompiler)) {
+		return new PostcssCompiler(inputTrees, inputFile, outputFile, plugins, map);
+	}
 
-    if ( !Array.isArray(inputTrees) ) {
-        throw new Error('Expected array for first argument - did you mean [tree] instead of tree?');
-    }
+	if (!Array.isArray(inputTrees)) {
+		throw new Error('Expected array for first argument - did you mean [tree] instead of tree?');
+	}
 
-    CachingWriter.call(this, inputTrees, inputFile);
+	CachingWriter.call(this, inputTrees, inputFile);
 
-    this.inputFile = inputFile;
-    this.outputFile = outputFile;
-    this.plugins = plugins || [];
-    this.map = map || {};
-    this.warningStream = process.stderr;
+	this.inputFile = inputFile;
+	this.outputFile = outputFile;
+	this.plugins = plugins || [];
+	this.map = map || {};
+	this.warningStream = process.stderr;
+    this.useSass = useSass || false;
+	this.includePaths = includePaths || [];
 }
 
 PostcssCompiler.prototype = Object.create(CachingWriter.prototype);
 PostcssCompiler.prototype.constructor = PostcssCompiler;
 
-PostcssCompiler.prototype.updateCache = function (includePaths, destDir) {
-    var toFilePath = path.join(destDir, this.outputFile);
-    var fromFilePath = path.join(includePaths[0], this.inputFile);
+PostcssCompiler.prototype.updateCache = function(includePaths, destDir) {
+	var toFilePath = path.join(destDir, this.outputFile);
+	var fromFilePath = path.join(includePaths[0], this.inputFile);
 
-    if ( !this.plugins || this.plugins.length < 1 ) {
-        throw new Error('You must provide at least 1 plugin in the plugin array');
+	if (!this.plugins || this.plugins.length < 1) {
+		throw new Error('You must provide at least 1 plugin in the plugin array');
+	}
+
+    var css = fs.readFileSync(fromFilePath, 'utf8');
+
+
+    //Sass compiling first!
+    if(this.useSass) {
+		var importPath = path.join(__dirname, "../..", "app/styles");
+
+		var incPaths = this.includePaths.concat(importPath);
+
+        css = sass.renderSync({
+    		data: css,
+			includePaths: incPaths
+    	}).css;
     }
 
-    var processor = postcss();
-    var css = fs.readFileSync(fromFilePath, 'utf8');
-    var options = {
-        from: fromFilePath,
-        to: toFilePath,
-        map: this.map
-    };
+    //postcss stuff
+	var processor = postcss();
 
-    this.plugins.forEach(function (plugin) {
-        var pluginOptions = assign(options, plugin.options || {});
-        processor.use(plugin.module(pluginOptions));
-    });
+	var options = {
+		from: fromFilePath,
+		to: toFilePath,
+		map: this.map
+	};
 
-    var warningStream = this.warningStream;
+	this.plugins.forEach(function(plugin) {
+		var pluginOptions = assign(options, plugin.options || {});
+		processor.use(plugin.module(pluginOptions));
+	});
 
-    return processor.process(css, options)
-        .then(function (result) {
-            result.warnings().forEach(function (warn) {
-                warningStream.write(warn.toString());
-            });
+	var warningStream = this.warningStream;
 
-            mkdirp.sync(path.dirname(toFilePath));
-            fs.writeFileSync(toFilePath, result.css);
-        })
-        .catch(function (error) {
-            if ( 'CssSyntaxError' === error.name ) {
-                error.message += "\n" + error.showSourceCode();
-            }
+	return processor.process(css, options)
+		.then(function(result) {
+			result.warnings().forEach(function(warn) {
+				warningStream.write(warn.toString());
+			});
 
-            throw error;
-        });
+			mkdirp.sync(path.dirname(toFilePath));
+			fs.writeFileSync(toFilePath, result.css);
+		})
+		.catch(function(error) {
+			if ('CssSyntaxError' === error.name) {
+				error.message += "\n" + error.showSourceCode();
+			}
+
+			throw error;
+		});
 };
 //end of rip
 
@@ -80,8 +100,6 @@ PostcssCompiler.prototype.updateCache = function (includePaths, destDir) {
 function PostCSSPlugin(optionsFn) {
 	this.name = 'ember-cli-postcss';
 	this.optionsFn = optionsFn;
-	//this.plugins = options.plugins;
-	//this.map = options.map;
 }
 
 PostCSSPlugin.prototype.toTree = function(tree, inputPath, outputPath, inputOptions) {
@@ -92,27 +110,27 @@ PostCSSPlugin.prototype.toTree = function(tree, inputPath, outputPath, inputOpti
 		inputTress = inputTrees.concat(options.includePaths);
 	}
 
-    var trees = Object.keys(options.outputPaths).reduce(function(trees, file) {
-        var input;
-        if (options.extension) {
-            input = path.join('.', inputPath, file + '.' + options.extension);
-        } else {
-            input = tryFile(file + '.css') || tryFile(file + '.css');
-        }
+	var trees = Object.keys(options.outputPaths).reduce(function(trees, file) {
+		var input;
+		if (options.extension) {
+			input = path.join('.', inputPath, file + '.' + options.extension);
+		} else {
+			input = tryFile(file + '.css') || tryFile(file + '.css');
+		}
 
-        var output = options.outputPaths[file];
-        if (input) {
-            trees.push(new PostcssCompiler(inputTrees, input, output, options.plugins, options.map));
-        }
-        return trees;
-    }, []);
+		var output = options.outputPaths[file];
+		if (input) {
+			trees.push(new PostcssCompiler(inputTrees, input, output, options.plugins, options.map, options.useSass, options.includePaths));
+		}
+		return trees;
+	}, []);
 
-    function tryFile(file) {
-        var filePath = path.join('.', inputPath, file);
-        return fs.existsSync(filePath) ? filePath : false;
-    }
+	function tryFile(file) {
+		var filePath = path.join('.', inputPath, file);
+		return fs.existsSync(filePath) ? filePath : false;
+	}
 
-    return mergeTrees(trees);
+	return mergeTrees(trees);
 };
 
 module.exports = {
@@ -120,28 +138,28 @@ module.exports = {
 	shouldSetupRegistryInIncluded: function() {
 		return !checker.isAbove(this, '0.2.0');
 	},
-    postcssOptions: function(){
-        var env  = process.env.EMBER_ENV;
-        var options = (this.app && this.app.options.postcssOptions) || {};
-        var envConfig = this.project.config(env).sassOptions;
-        if (envConfig) {
-            console.warn("Deprecation warning: PostcssOptions should be moved to your ember-cli-build");
-            merge(options, envConfig);
-        }
+	postcssOptions: function() {
+		var env = process.env.EMBER_ENV;
+		var options = (this.app && this.app.options.postcssOptions) || {};
+		var envConfig = this.project.config(env).sassOptions;
+		if (envConfig) {
+			console.warn("Deprecation warning: PostcssOptions should be moved to your ember-cli-build");
+			merge(options, envConfig);
+		}
 
-        if (options.map) {
-            // we need to embed the sourcesContent in the source map until libsass has better support for broccoli-sass
-            options.map = true;
-        }
+		if (options.map) {
+			// we need to embed the sourcesContent in the source map until libsass has better support for broccoli-sass
+			options.map = true;
+		}
 		options.plugins = options.plugins || [];
-        
+
 		options.outputFile = options.outputFile || this.project.name() + '.css';
 
-        return options;
-    },
-    setupPreprocessorRegistry: function(type, registry) {
-        registry.add('css', new PostCSSPlugin(this.postcssOptions.bind(this)));
-    },
+		return options;
+	},
+	setupPreprocessorRegistry: function(type, registry) {
+		registry.add('css', new PostCSSPlugin(this.postcssOptions.bind(this)));
+	},
 	included: function included(app) {
 		this.app = app;
 		this._super.included.apply(this, arguments);
